7d90308a90bb451ec37759975e304470cbfa264e
japerezcarri <54781818+japerezcarri@users.noreply.github.com>
2020-06-10 09:55:55 -0500

    Revert "canciones, control, modelo"
    This reverts commit b7be654fe5e0e929665a944ac7065b6b81c3abae.

--- back/app.js
+++ back/app.js
@@ -1,17 +1,21 @@
-const express= require('express');
-const app=express();
-//declaracion cors
-const cors=require('cors');
-//variable de rutas a ejecutar
-const usuarioRutas= require('./rutas/usuarioRutas');
-const cancionRutas= require('./rutas/cancionRutas');
+const express = require('express');
+const app = express();
+
+// Declaración de cors
+const cors = require('cors');
+
+// Declaración de la constante delas rutas de usuarios
+const usuarioRutas = require('./rutas/usuarioRutas');
 
 
-//MIDLEWARES
+// -- MIDDLEWARES --
 app.use(express.json());
 app.use(cors());
-//consumo de rutas
-app.use('/api',usuarioRutas);
-app.use('/api',cancionRutas);
 
-module.exports=app;
+// Consumo de rutas
+app.use('/api', usuarioRutas)
+
+// --FIN MIDDLEWARES --
+
+// Exportación del modulo
+module.exports = app;
